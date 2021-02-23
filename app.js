797855b379f727235cbf454da71b22a937413a55
maunonen <santari33@gmail.com>
2021-02-23 11:53:07 +0200

    comment

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(bodyParser.json())
 app.use(middleware.regLogger)
 
 //Routers goes here 
-
+/* some comment */
+/* some comment */
 
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
